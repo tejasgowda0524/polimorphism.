1530f82afebca4b01d571dc9cdc35c672e36328d
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 20:53:51 +0530

    main method

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace polimorphim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cargoplane c = new cargoplane();
+            passengerplane p = new passengerplane();
+            fighterplane f = new fighterplane();
+
+            airport.permit(c);
+            airport.permit(p); 
+            airport.permit(f);
+        }
+    }
+}
